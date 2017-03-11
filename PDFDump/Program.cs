1b9fad0b29db16315e9f86c054a89f6bd2aa60f9
Mica Funston <mica.funston@gmail.com>
2017-03-11 15:32:22 -0500

    Initial. Working, but no exception checks.

--- PDFDump/Program.cs
+++ PDFDump/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
 
 namespace PDFDump
 {
@@ -10,6 +11,56 @@ namespace PDFDump
     {
         static void Main(string[] args)
         {
+            ProcessPDFs(GetPDFList());
+            //string fileName;
+
+            //fileName = @"Resume_MicaFunston.pdf";
+            
+            //var results = GetTextFromAllPages(fileName);
+            //WriteText(fileName, results);
+        }
+
+        public static List<string> GetPDFList()
+        {
+            List<String> PDFs = new List<string>();
+            var folder = new DirectoryInfo(System.IO.Directory.GetCurrentDirectory());
+            foreach (var fi in folder.EnumerateFiles("*.pdf"))
+            {
+                PDFs.Add(fi.Name);
+            }
+            return PDFs;
+        }
+
+        public static void ProcessPDFs(List<string> PDFList)
+        {
+            foreach(var pdf in PDFList)
+            {
+                var txt = GetTextFromAllPages(pdf);
+                WriteText(pdf, txt);
+            }
+        }
+
+        public static void WriteText(string fileName, string textToWrite)
+        {
+            string txtFile = fileName.Remove(fileName.Length - 4);
+            txtFile = txtFile + ".txt";
+
+            var file = System.IO.File.CreateText(@txtFile);
+            file.Write(textToWrite);
+            file.Dispose();
+        }
+
+        public static string GetTextFromAllPages(String pdfPath)
+        {
+            PdfReader reader = new PdfReader(pdfPath);
+
+            StringWriter output = new StringWriter();
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy()));
+
+            return output.ToString();
         }
     }
+
 }
